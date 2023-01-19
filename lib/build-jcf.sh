4b660cd94ebe426d1b59bbbb4ba6a0b252ae447b
Bogen85 <das@teegra.net>
2023-01-19 14:21:19 -0600

    lazbuild not finding ppcx64 on freebsd

--- lib/build-jcf.sh
+++ lib/build-jcf.sh
@@ -6,7 +6,7 @@ echo '@@ Build Jedi Code Formatter (JCF)'
   export PATH=$SHARED_FPC_BIN_:$SHARED_FPC_BASE_:$PATH
   export PATH=$SHARED_LAZ_USR_/bin:$PATH
 
-  lazbuild --quiet --lazarusdir=$SHARED_LAZARUS_DIR_ jcf.lpr |\
+  lazbuild --quiet --compiler=$SHARED_FPC_USR_/lib/fpc/3.3.1/ppcx64 --lazarusdir=$SHARED_LAZARUS_DIR_ jcf.lpr |\
     grep -Ev '(Linking |Start compiling package|warning: |Warning: |Hint: |Note: |Writing Resource String| not found)' |\
     grep -Ev '(Free Pascal Compiler version |Copyright |\(s\) issued| lines compiled.|Target OS: )'
 
