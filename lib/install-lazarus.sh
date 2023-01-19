57e2cc824401e1b4760321f1955de11e4ff8da32
Bogen85 <das@teegra.net>
2023-01-19 12:41:56 -0600

    Accomadating freebsd

--- lib/install-lazarus.sh
+++ lib/install-lazarus.sh
@@ -7,9 +7,18 @@ echo '@@ Install Lazarus'
 	git describe
 
 	rm -rf ${SHARED_LAZ_USR_}
+	mkdir -pv ${SHARED_LAZ_USR_}/local/lazarus
 
 	$make --silent install INSTALL_PREFIX=${SHARED_LAZ_USR_} |\
 	(grep -Ev '(Installing |Start compiling package | not found|Compiled package )' || true)
+
+	SHARE_LAZ=${SHARED_LAZ_USR_}/share/lazarus
+	if [ -d ${SHARE_LAZ}/components ]; then
+		:
+	else
+		rmdir -v ${SHARE_LAZ}
+		ln -svf ${SHARED_LAZ_USR_}/local/lazarus ${SHARE_LAZ}
+	fi
 
 	git describe
 	true
