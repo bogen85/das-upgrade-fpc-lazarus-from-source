88f712949be8fd94df210b15509ddf710815cb63
Dwight Schauer <dwight.schauer@teegra.net>
2023-08-22 09:35:28 -0500

    Added full git urls so they would not be complained about

--- lib/clone-fpc-gitlab-repo.sh
+++ lib/clone-fpc-gitlab-repo.sh
@@ -3,6 +3,6 @@ echo '@@ Clone FPC gitlab repo into cache'
 (
 	set -euo pipefail
 	main_cache="${SHARED_FPC_GIT_}/fpc_source_main_cache"
-	git_url="https://gitlab.com/freepascal.org/fpc/source"
+	git_url="https://gitlab.com/freepascal.org/fpc/source.git/"
 	__update_or_clone "${main_cache}" "${git_url}"
 )
--- lib/clone-lazarus-gitlab-repo.sh
+++ lib/clone-lazarus-gitlab-repo.sh
@@ -3,6 +3,6 @@ echo '@@ Clone Lazarus gitlab repo into cache'
 (
 	set -euo pipefail
 	main_cache="${SHARED_FPC_GIT_}/lazarus-main-cache"
-	git_url="https://gitlab.com/freepascal.org/lazarus/lazarus.git"
+	git_url="https://gitlab.com/freepascal.org/lazarus/lazarus.git/"
 	__update_or_clone "${main_cache}" "${git_url}"
 )
