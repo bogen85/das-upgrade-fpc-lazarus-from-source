6d0f2caaa390f526abf97600c8233cf9d789c8c2
Dwight Schauer <dwight.schauer@teegra.net>
2023-08-23 18:32:46 -0500

    Corrected JCF installer.

--- lib/install-jcf.sh
+++ lib/install-jcf.sh
@@ -1,9 +1,9 @@
-echo '@@ Build Jedi Code Formatter (JCF)'
+echo '@@ Install Jedi Code Formatter (JCF)'
 
 (
   JCF=$(find $SHARED_LAZ_USR_ | grep '/jcf$')
   mkdir -pv $SHARED_DEV_BIN_
-  rm -fv $SHARED_DEV_BIN_/$JCF
+  rm -fv $SHARED_DEV_BIN_/jcf
   cp -v $JCF $SHARED_DEV_BIN_/  
   true
 )
