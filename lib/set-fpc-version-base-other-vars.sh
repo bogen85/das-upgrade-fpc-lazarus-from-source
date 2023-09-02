5038e1375bd288a3171c2e68bcebfa05ab7bc615
bogen85 <bogen85@teegra.net>
2023-09-01 19:47:07 -0500

    Now setting ppc based on architecture.

--- lib/set-fpc-version-base-other-vars.sh
+++ lib/set-fpc-version-base-other-vars.sh
@@ -8,3 +8,15 @@ export SHARED_FPC_BIN_=${SHARED_FPC_USR_}/bin
 export SHARED_FPC_CFG_=${SHARED_FPC_ETC_}/fpc.cfg
 export SHARED_LAZARUS_DIR_=${SHARED_LAZ_USR_}/share/lazarus
 export SHARED_JCF_=${SHARED_LAZARUS_DIR_}/components/jcf2/Output/Lazarus/jcf
+
+export SHARED_FPC_PPC_BIN_=${SHARED_FPC_USR_}/lib/fpc/${SHARED_FPC_VERSION_}
+export SHARED_FPC_ARCH_=$(uname -m)
+
+
+[ "${SHARED_FPC_ARCH_}" == "aarch64" ] && SHARED_FPC_PPC_EXE_=${SHARED_FPC_PPC_BIN_}/ppca64
+[ "${SHARED_FPC_ARCH_}" == "x86_64"  ] && SHARED_FPC_PPC_EXE_=${SHARED_FPC_PPC_BIN_}/ppcx64
+
+export SHARED_FPC_PPC_EXE_
+
+printf 'FPC arch: %s\n' "${SHARED_FPC_ARCH_}"
+printf 'FPC  ppc: %s\n' "${SHARED_FPC_PPC_EXE_}"
