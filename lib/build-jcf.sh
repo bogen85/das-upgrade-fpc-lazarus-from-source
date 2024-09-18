8d5192c0a3d4b19bb5020ac60b1fecab2f239eff
Dwight Schauer <dwight.schauer@teegra.net>
2024-09-18 18:59:30 -0500

    Now building jcfbase before building jcf

--- lib/build-jcf.sh
+++ lib/build-jcf.sh
@@ -2,13 +2,26 @@ echo '@@ Build Jedi Code Formatter (JCF)'
 
 (
   set -x
-  cd $SHARED_LAZ_USR_/share/lazarus/components/jcf2/CommandLine/Lazarus
+
   export PATH=$SHARED_FPC_BIN_:$SHARED_FPC_BASE_:$PATH
   export PATH=$SHARED_LAZ_USR_/bin:$PATH
-
   ppc=${SHARED_FPC_PPC_EXE_}
+  lazbuild="lazbuild --quiet --compiler=$ppc --lazarusdir=$SHARED_LAZARUS_DIR_"
+  jcf2="/share/lazarus/components/jcf2"
 
-  lazbuild --quiet --compiler=$ppc --lazarusdir=$SHARED_LAZARUS_DIR_ jcf.lpr |\
+  cd $SHARED_LAZ_USR_$jcf2
+
+  rm -rf lib/* || true
+
+  $lazbuild jcfbase.lpk |\
+    grep -Ev '(Linking |Start compiling package|warning: |Warning: |Hint: |Note: |Writing Resource String| not found)' |\
+    grep -Ev '(Free Pascal Compiler version |Copyright |\(s\) issued| lines compiled.|Target OS: )'
+
+  cd $SHARED_LAZ_USR_$jcf2/CommandLine/Lazarus
+
+  rm -rf lib/* || true
+
+  $lazbuild jcf.lpi |\
     grep -Ev '(Linking |Start compiling package|warning: |Warning: |Hint: |Note: |Writing Resource String| not found)' |\
     grep -Ev '(Free Pascal Compiler version |Copyright |\(s\) issued| lines compiled.|Target OS: )'
 
